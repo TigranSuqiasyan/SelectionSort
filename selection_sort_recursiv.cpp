24735f2d90c5d504298dcf9bdd41390c522389e6
TigranSuqiasyan <tigransuqiasyan.2001@gmail.com>
2022-06-29 20:47:03 +0400

    optimization added

--- selection_sort_recursiv.cpp
+++ selection_sort_recursiv.cpp
@@ -1,12 +1,11 @@
-#include <iostream>
 #include <vector>
-#include <ctime>
 
 template <typename T>
 void selection_sort(std::vector<T>& vec, int index)
 {
     if(index == vec.size() - 1)
         return;
+    bool is_sorted = true;
     T temp{};
     int size = vec.size();
     int min_index = index;
@@ -14,26 +13,13 @@ void selection_sort(std::vector<T>& vec, int index)
     {
         if(vec[i] < vec[min_index])
             min_index = i;
+        if(vec[i] < vec[i - 1])
+            is_sorted = false;
     }
     temp = vec[min_index];
     vec[min_index] = vec[index];
     vec[index] = temp;
+    if(is_sorted)
+        return;
     selection_sort(vec, index + 1);
-}
-
-int main()
-{
-    srand(time(0));
-    std::vector<int> vec(100);
-    for(int i{}; i < 100; ++i)
-    {
-        vec[i] = rand() % 100;
-    }
-    selection_sort(vec, 0);
-    for(int i{}; i < 100; ++i)
-    {
-        if(!(i % 10))
-            std::cout << "\n";
-        std::cout << vec[i] << " ";
-    }
 }
